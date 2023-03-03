bf8735a2bcdd42402240e159fbf722e8e6e096aa
apophis51 <malcolmxvernon@hotmail.com>
2023-03-03 15:51:15 -0500

    sfsf

--- bash.js
+++ bash.js
@@ -3,9 +3,17 @@ process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim(); //remove the newline
+    if (data === 'pwd'){
+    console.log(process.cwd()); //remove the newline
+    }
+    else {
+        console.log("bad command")
+    }
 
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt > ');
 });
 
+
+
+// "console.log(process.cwd()"
